387fa4cda59bdc01c394e768437031594756e1bd
s-bot12 <lkns0989@gmail.com>
2021-10-12 17:51:22 +0900

    21.10.12

--- addition-game-starter/truffle.js
+++ addition-game-starter/truffle.js
@@ -3,7 +3,7 @@ const PrivateKeyConnector = require('connect-privkey-to-provider')
 const NETWORK_ID = '1001'
 const GASLIMIT = '20000000'
 const URL = 'https://api.baobab.klaytn.net:8651'
-const PRIVATE_KEY = '0xccb08271eb3d26fa32a2db04a27ccbbbf86d0f5aa2b63d53904a09fc9227226a'
+const PRIVATE_KEY = '<-- personal key -->'
 
 module.exports = {
     networks: {
